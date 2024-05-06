ed16e3a60151852d984a8bd677f664b9ef513910
vj-sonawane <vijayshreeacademy20@gmail.com>
2024-05-07 00:16:07 +0530

    Logback.xml and Redis Configuration

--- src/main/java/com/vscode/springbootjournalappmongodb/config/TransactionConfig.java
+++ src/main/java/com/vscode/springbootjournalappmongodb/config/TransactionConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -22,4 +25,12 @@ public class TransactionConfig {
         return new RestTemplate();
     }
 
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        return  redisTemplate;
+    }
 }
